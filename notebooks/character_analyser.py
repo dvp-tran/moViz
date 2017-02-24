c52f00496fe574974efa7ccdfefd8e322b1a4d17
Paul <dvp.tran@gmail.com>
2017-02-24 23:32:38 +0100

    update character analysis package"

--- notebooks/character_analyser.py
+++ notebooks/character_analyser.py
@@ -51,3 +51,134 @@ def get_script(url):
     return script_text
 
 
+def clean_character(char):
+    if isinstance(char,unicode) or isinstance(char,str):
+        char=char.replace('\n','').replace('*','').replace('O/S','').replace('V.O.','')
+        char=re.sub(' +',' ',char)
+        if 'CONTINUED' in char:
+            return u'CONTINUED'
+        if '(' in char or ')' in char:
+            regex = re.compile(".*?\((.*?)\)")
+            to_delete = re.findall(regex, char)
+            for e in to_delete:
+                char=char.replace('(%s)' %e,'')
+            if char=='' or ')'in char:
+                return None
+            else:
+                return char.rstrip() #delete lose end white space
+        else:
+            return char.rstrip()
+    else:
+        return None
+    
+def continued_speech(char_vector):
+    char_vector=list(char_vector)
+    while 'CONTINUED' in char_vector:
+        count=0
+        idx=char_vector.index("CONTINUED")
+        for element in char_vector[idx-1::-1]:
+            #if (isinstance(element,str) or isinstance(element,unicode)) and (element!='unknown'):
+            if element and element!='unknown':
+                char_vector[idx]=char_vector[idx-1-count]
+                #print('Replaces CONTINUED with %s' %(char_vector[idx-1-count]))
+                break
+            count=count+1   
+    return char_vector
+
+
+# to remove outliers
+def principal_characters(char_dic):
+    mean = 0
+    for i, j in char_dic.items():
+        mean = mean + j
+    mean = mean / len(char_dic)
+
+    stddev = 0
+    for i, j in char_dic.items():
+        stddev = stddev + (j - mean)*(j - mean)
+    stddev = stddev / len(char_dic)
+    stddev = math.sqrt(stddev)
+
+    temp = {}
+    for i, j in char_dic.items():
+        if (math.fabs(j - mean) < 3*stddev):
+            temp[i] = j
+    print(stddev,mean)
+    return temp
+
+
+def char_analysis(df):
+    d=dict(Counter(df['character_clean']))
+    d={key: value for key, value in d.items() 
+                 if key}
+    charlist = sorted(d)
+    gender = dict()
+    lines=[element.split('\n') for element in output[1]['text']]
+    lines=[item for sublist in lines for item in sublist]
+
+    for char in charlist:
+        m, f, p = 0, 0, 0 
+        for i in range(len(lines)):
+            srch = re.search(char, lines[i], re.I)
+            if not srch:
+                continue
+            if srch.group().isupper():
+                continue
+            else:
+                k, k_range = -1, 2
+                while k + i + 1 < len(lines):
+                    k = k + 1
+                    m = m + len(re.findall("\s+he", lines[k+i], re.I)) + len(re.findall("\s+him", lines[k+i], re.I))
+                    f = f + len(re.findall("\s+she", lines[k+i], re.I)) + len(re.findall("\s+her", lines[k+i], re.I))
+                    if lines[k+i].isupper():
+                        break
+                    if k >= k_range:
+                        break
+                k, k_range = -1, 4
+                while k + i + 1 < len(lines):
+                    k = k + 1
+                    m1, m2, m3, m4 = re.search("\s+he", lines[k+i], re.I), re.search("\s+him", lines[k+i], re.I),\
+                                    re.search("\s+she", lines[k+i], re.I), re.search("\s+her", lines[k+i], re.I)
+                    if m1:
+                        if m2:
+                            mc = min(m1.span()[0], m2.span()[0])
+                        else:
+                            mc = m1.span()[0]
+                    else:
+                        mc = len(lines[k+i])
+                    if m3:
+                        if m4:
+                            fc = min(m3.span()[0], m4.span()[0])
+                        else:
+                            fc = m3.span()[0]
+                    else:
+                        fc = len(lines[k+i])
+                    if not(m1 or m2 or m3 or m4):
+                        continue
+                    else:
+                        if (mc < fc):
+                            p = p - 1
+                        else:
+                            p = p + 1
+                        break
+                    if lines[k+i].isupper():
+                        break
+                    if k >= k_range:
+                        break
+        gender[char] = m, f, p
+
+    for char, g in gender.items():
+        g_score = 2*g[1] - g[0] 
+        g_sum = g[1]+g[0]
+        g_score = g_score
+        if (g_sum == 0.0) :#or (0 == 1):
+            #print(char+" Undetermined ")
+            gender[char] = "?" #0, 0, -1
+        elif g_score > 0:
+            #print(char+" Female "+str(g[0])+","+str(g[1])+","+str(g[2])+","+str(g_score))
+            gender[char] = "F" #0, 0, 1
+        else:
+            #print(char+" Male", " ", str(g[0])+","+str(g[1])+","+str(g[2])+","+str(g_score))
+            gender[char] = "M" #0, 0, 0
+            
+    return d,gender
